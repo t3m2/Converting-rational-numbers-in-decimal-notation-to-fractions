103ea8977be68c9a00ed85b2e016fc3eb3383826
t3m2 <46426496+t3m2@users.noreply.github.com>
2019-12-14 13:28:09 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import converts_decimals_to_fractions
+import convert_decimals_to_fractions
 
 print("This program turns \"any\" rational number in decimal notation \
 into a fraction, for example: -2.5(37) = -2.537373737373737... = -1256/495\n\n")
@@ -8,7 +8,7 @@ while 1:
     x = input("Enter a rational number in decimal notation (exit: 'b'): ")
     if x == 'b':
       break
-    print("%s = %s" % (x, converts_decimals_to_fractions.from_string_to_fraction(x)))
+    print("%s = %s" % (x, convert_decimals_to_fractions.from_string_to_fraction(x)))
   except:
     print("Error: probably, invalid input.")
 
